108f5ff08c3ce040d4c74b5287cddb57d9941ab9
Jucinei <69520602+jucineibelo@users.noreply.github.com>
2024-07-16 20:06:59 -0300

    Delete API-futebol directory

--- API-futebol/src/main/java/com/meli/futebol/exceptons/InvalidInputException.java
+++
@@ -1,7 +0,0 @@
-package com.meli.futebol.exceptons;
-
-public class InvalidInputException extends RuntimeException {
-    public InvalidInputException(String message) {
-        super(message);
-    }
-}
--- API-futebol/src/main/java/com/meli/futebol/exceptons/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.meli.futebol.exceptons;
-
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
