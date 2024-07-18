e4d4dbdd257f17a4b3ff1563612e7f738e1eee95
Jucinei <69520602+jucineibelo@users.noreply.github.com>
2024-07-17 21:01:58 -0300

    Delete API-futebol directory

--- API-futebol/src/main/java/com/meli/futebol/exceptions/DateInvalidException.java
+++
@@ -1,7 +0,0 @@
-package com.meli.futebol.exceptions;
-
-public class DateInvalidException extends RuntimeException{
-    public DateInvalidException(String message) {
-        super(message);
-    }
-}
